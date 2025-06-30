934a30cc6bc46c7681bd01976d484f6004ecaae3
Kaua Gigante <Kauagigantesmarsi@gmail.com>
2025-06-30 09:04:12 -0300

    Função de msg OK

--- config.js
+++ config.js
@@ -2,5 +2,9 @@ function soma(n1 = 0, n2 = 0) {
     return n1 + n2;
 }
 function multiplica(ni = 0, n2 =0){
-    return n1 * n2
-}   
+    return n1 * n2;
+} 
+
+function mensagem(msg = "Ola", name = "kacalo"){
+    alert(msg + " " + name + "!!!")
+}
